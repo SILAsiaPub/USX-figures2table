cc70ae9891c4b91576be48a3318332603f054cc1
indiamcq <india.mcq@gmail.com>
2017-07-19 19:10:55 +1000

    missing project.txt

--- run.cmd
+++ run.cmd
@@ -3,6 +3,7 @@ set saxon9=saxon9he.jar
 set projxslt=project.xslt
 set var2x=variable2xslt.xslt
 call :variableslist project.txt
+call :testjava
 if not exist "%ProgramFiles%\java" echo is java installed? & pause & exit
 if exist "%projxslt%" del "%projxslt%"
 @echo on
@@ -20,4 +21,17 @@ goto :eof
 :: list - a filename with name=value on each line of the file
 set list=%~1
 FOR /F "eol=[ delims== tokens=1,2" %%s IN (%list%) DO set %%s=%%t
+goto :eof
+
+:testjava
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`where java`) DO (
+  SET var!count!=%%F
+  SET /a count=!count!+1
+)
+ENDLOCAL
+ECHO %var1%
+ECHO %var2%
+ECHO %var3%
 goto :eof
